4b08f83acfe176d680b6e087280f742ab93c1b3a
Jeff <jcgilbert70@gmail.com>
2023-01-09 20:40:39 -0600

    initial models push

---
+++ models/Reviews.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Reviews extends Model {}
+
+
+Reviews.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    location_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    review: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'reviews'
+  }
+);
+
+module.exports = Reviews;
--- models/index.js
+++ models/index.js
@@ -1,1 +1,42 @@
-module.exports = {};
+const Users = require('./Users');
+const Locations = require('./Locations');
+const Ratings = require('./Ratings');
+const Reviews = require('./Reviews');
+const Tags = require('./Tags');
+const LocationTags = require('./LocationTags');
+
+Locations.hasMany(Tags, {
+    foreignKey: 'tags_id',
+});
+Locations.hasMany(Reviews, {
+    foreignKey: 'reviews_id',
+});
+
+
+Reviews.belongsTo(Locations, {
+    foreignKey: 'locations_id',
+});
+Reviews.belongsTo(Users, {
+    foreignKey: 'users_id',
+});
+Reviews.hasMany(Tags, {
+    foreignKey: 'tags_id',
+    through: LocationTags,
+});
+
+
+Tags.belongsTo(Locations, {
+    foreignKey: 'locations_id',
+});
+Tags.hasMany(Reviews, {
+    foreignKey: 'reviews_id',
+    through: LocationTags,
+});
+
+
+Users.hasMany(Reviews, {
+    foreignKey: 'reviews_id',
+});
+
+
+module.exports = { Users, Locations, Ratings, Reviews, Tags, LocationTags };
