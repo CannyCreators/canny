033f29479182f2a9c7c391974c2db8cc0e176a3b
Andrew Knoedler <andrewknoedler89@gmail.com>
2023-01-10 20:54:11 -0600

    Update locationsRoutes.js

--- controllers/api/locationsRoutes.js
+++ controllers/api/locationsRoutes.js
@@ -1,46 +1,46 @@
 const router = require('express').Router();
 const path = require('path');
-const { Locations, Reviews, Tags } = require('../../models');
+// const { Locations, Reviews, Tags } = require('../../models');
 
-router.get('/', async (req, res) => {
-    const locationsDB = await Locations.findAll({
-        include: [
-            {
-                model: Tags,
-                attributes: [
-                    'id',
-                    'tag_name'
-                ]
-            }
-        ]
-    });
-    const allLocations = locationsDB.get({plain: true});
-    res.render('locationSearch', {allLocations})
-})
+// router.get('/', async (req, res) => {
+//     const locationsDB = await Locations.findAll({
+//         include: [
+//             {
+//                 model: Tags,
+//                 attributes: [
+//                     'id',
+//                     'tag_name'
+//                 ]
+//             }
+//         ]
+//     });
+//     const allLocations = locationsDB.get({plain: true});
+//     res.render('locationSearch', {allLocations})
+// })
 
-router.get('/:id', async (req, res) => {
-    const searchedLocation = await Locations.findByPk(req.params.id, {
-        include: [
-            {
-                model: Reviews,
-                attributes: [
-                    'id',
-                    'location_name',
-                    'review',
-                    ''
-                ]
-            },
-            {
-                model: Tags,
-                attributes: [
-                    'id',
-                    'tag_name'
-                ]
-            }
-        ]
-    });
-    const locationDisplay = searchedLocation.get({plain: true});
-    res.render('locationDisplay', {locationDisplay});
-})
+// router.get('/:id', async (req, res) => {
+//     const searchedLocation = await Locations.findByPk(req.params.id, {
+//         include: [
+//             {
+//                 model: Reviews,
+//                 attributes: [
+//                     'id',
+//                     'location_name',
+//                     'review',
+//                     ''
+//                 ]
+//             },
+//             {
+//                 model: Tags,
+//                 attributes: [
+//                     'id',
+//                     'tag_name'
+//                 ]
+//             }
+//         ]
+//     });
+//     const locationDisplay = searchedLocation.get({plain: true});
+//     res.render('locationDisplay', {locationDisplay});
+// })
 
 module.exports = router;
