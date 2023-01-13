08e2fd6e52800d977ad4802017eb6d79a229912c
genmla <gmmladenka@gmail.com>
2023-01-12 19:42:33 -0600

    added simple post to review routes, card to submit review to review handlebars and js for adding review

---
+++ controllers/api/reviewRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const path = require('path');
+const { Reviews } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const createReview = await Reviews.create({
+            location_name: req.body.location_name,
+            title: req.body.title,
+            review: req.body.review,
+        });
+        res.status(200).json(createReview)
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
