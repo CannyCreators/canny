bac1313157fc8fb5db60ce5895b95e130ec9dd35
aknoedler <andrewknoedler89@gmail.com>
2023-01-21 22:55:18 -0600

    took out tests for IP lookup on heroku

--- public/js/homepage.js
+++
@@ -1,9 +0,0 @@
-const loadWithIPData = async () => {
-    const ipData = await fetch("https://ipwho.is/");
-    const ip = await ipData.json();
-    
-    const userIP = ip.ip;
-    alert(userIP);
-}
-
-loadWithIPData();
